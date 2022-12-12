3379caf794a2d804dd4a59e4c5a872939e4b2c1c
Kenny Hung (he) <khung@instructors.2u.com>
2022-12-12 18:45:44 +0000

    Third passing test

--- match.test.js
+++ match.test.js
@@ -16,11 +16,16 @@ describe('match function', () => {
     
     expect(actual).toBe(expected)
   })
+  
+  it('should match third test case', () => {
+    const expected = true
+    
+    const actual = match([1, 2, 3], function(num) { return num % 2 === 0 })
+    
+    expect(actual).toBe(expected)
+  })
 })
 /*
-Input: [1,5,3]  function(num){return num === 2}
-Output: false
-
 Input: [1,2,3]  function(num){return num % 2 === 0}
 Output: true
 
